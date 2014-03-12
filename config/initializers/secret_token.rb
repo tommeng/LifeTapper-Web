7c60dc94af2e22023ac5533ac9b161346d22dce8
Thomas Meng <tmeng999@gmail.com>
2014-03-12 16:09:20 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LifeTapperWeb::Application.config.secret_key_base = 'b7cb9b6ce8a295be9ee691683945efa86b18651075aea43ee74fc952ab7f1f8c25117f79683f4a3ff5cc696aaf1ab1a600af411027b1142a7ed593f69ee55b4d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LifeTapperWeb::Application.config.session_store :cookie_store, key: '_LifeTapper-Web_session'
