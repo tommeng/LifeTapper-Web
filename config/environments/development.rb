8fc4a7f427bc2bc4df8969de74d6dfcd2b4ff0b8
Thomas Meng <tmeng999@gmail.com>
2014-04-07 17:40:18 -0400

    updated configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,12 +31,11 @@ LifeTapperWeb::Application.configure do
   config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'}
 
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
+  address: "smtp.sendgrid.net",
+  port: 587,
+  domain: "lifetapper.herokuapp.com",
+  authentication: :plain,
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD']
+}
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,12 +80,11 @@ LifeTapperWeb::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: "lifetapper.herokuapp.com",
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD']
   }
 end
