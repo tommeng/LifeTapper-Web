d73f49e47149561dc5f323b804b9279550e0e236
Thomas Meng <tmeng999@gmail.com>
2014-04-07 17:12:51 -0400

    added mail routing to heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,5 +28,5 @@ LifeTapperWeb::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'lifetapper.herokuapp.com' }
 end
